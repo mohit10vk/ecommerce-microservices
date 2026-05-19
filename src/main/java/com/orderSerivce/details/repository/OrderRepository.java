6e4d14da7013d1f9735cd3ba90946dafb79ae225
mohit10vk <mohitvishavkarma7520@gmail.com>
2026-05-19 22:52:55 +0530

    Delete src directory

--- src/main/java/com/orderSerivce/details/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.orderSerivce.details.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.orderSerivce.details.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long>{
-
-}
