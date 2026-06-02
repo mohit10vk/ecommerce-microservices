4746db6e7b8f2204371d4d4d61c957637ef651e3
Mohit <mohitvishavkarma7520@gmail.com>
2026-06-02 18:40:10 +0530

    BCrypt password encryption

--- AuthService/src/main/java/com/auth/service/controller/AuthController.java
+++ AuthService/src/main/java/com/auth/service/controller/AuthController.java
@@ -1,13 +1,14 @@
 package com.auth.service.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.auth.service.config.SecurityConfig;
+
 import com.auth.service.entity.User;
 import com.auth.service.repository.UserRepository;
 import com.auth.service.service.JwtService;
@@ -16,36 +17,40 @@ import com.auth.service.service.JwtService;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final SecurityConfig securityConfig;
+  
 
     private final UserRepository userRepository;
 	
 	@Autowired
 	private JwtService jwtService;
-
-    AuthController(UserRepository userRepository, SecurityConfig securityConfig) {
-        this.userRepository = userRepository;
-        this.securityConfig = securityConfig;
-    }
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	public AuthController(UserRepository userRepository) {
+	    this.userRepository = userRepository;
+	}
     
     @PostMapping("/login")
     public String login(@RequestBody User user) {
 
         User dbUser = userRepository.findByUsername(user.getUsername());
+        if(dbUser != null && passwordEncoder.matches( user.getPassword(),
+        		       dbUser.getPassword()
+        		   )) {
 
-        if(dbUser != null &&
-           dbUser.getPassword().equals(user.getPassword())) {
-
-            return jwtService.generateToken(user.getUsername());
-        }
+        		    return jwtService.generateToken(user.getUsername());
+        		}
 
         return "Invalid Credentials";
     }
 	
 	@PostMapping("/register")
 	public User register(@RequestBody User user) {
-
+       
+		user.setPassword(
+		        passwordEncoder.encode(user.getPassword())
+		    );
 	    return userRepository.save(user);
 	}
 	
